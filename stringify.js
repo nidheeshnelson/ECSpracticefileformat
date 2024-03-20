7fbc46f20fd2ecb300978ba12d09a9d5b152b694
Nidheesh Nelson <nidheesh.n@ECSTVM-NIDHEESH.ecstvm.local>
2024-03-20 12:01:50 +0530

    person stringify

--- stringify.js
+++ stringify.js
@@ -2,3 +2,12 @@ const names = ["Nidheesh Nelson", "Braji UM", "Nidhin Nelson", "Preethi TP", "Ni
 const jsonString = JSON.stringify(names);
 console.log(names);
 console.log(jsonString);
+const person = {
+    "name": "Nidheesh Nelson",
+    "age": 32,
+    "isEmployed": true,
+    "hobbies": ["bible read", "study"]
+};
+const personstring = JSON.stringify(person);
+console.log(person);
+console.log(personstring)
