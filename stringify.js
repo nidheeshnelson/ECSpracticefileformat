825cb37cf2b72a64aa7939c27051133456473fc2
Nidheesh Nelson <nidheesh.n@ECSTVM-NIDHEESH.ecstvm.local>
2024-03-20 11:48:10 +0530

    first stringify commit

---
+++ stringify.js
@@ -0,0 +1,4 @@
+const names = ["Nidheesh Nelson", "Braji UM", "Nidhin Nelson", "Preethi TP", "Nimin Das", "Anila", "Nikhil Das"];
+const jsonString = JSON.stringify(names);
+console.log(names);
+console.log(jsonString);
